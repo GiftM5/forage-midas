aa35b6f7d17ca5a3fad68c247a056142386f2782
Mpho Mofokeng <mpmofokjhb024@studen.wethinkcode.co.za>
2025-05-16 15:35:53 +0200

    Added an interface(TransactionRepository)

--- src/main/java/com/jpmc/midascore/component/TransactionConduit.java
+++ src/main/java/com/jpmc/midascore/component/TransactionConduit.java
@@ -1,20 +1,18 @@
 package com.jpmc.midascore.component;
 
-
 import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.repository.TransactionRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TransactionConduit {
-    private final TransactionRepository transactionRepository;
+    private final TransactionRepository repo;
 
-    public TransactionConduit(TransactionRepository transactionRepository) {
-        this.transactionRepository = transactionRepository;
+    public TransactionConduit(TransactionRepository repo) {
+        this.repo = repo;
     }
 
     public void save(TransactionRecord record) {
-        transactionRepository.save(record);
+        repo.save(record);
     }
 }
-
--- src/main/java/com/jpmc/midascore/component/TransactionConsumer.java
+++ src/main/java/com/jpmc/midascore/component/TransactionConsumer.java
@@ -1,6 +1,5 @@
 package com.jpmc.midascore.component;
 
-
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jpmc.midascore.entity.TransactionRecord;
 import com.jpmc.midascore.foundation.Transaction;
@@ -10,28 +9,25 @@ import org.springframework.stereotype.Component;
 @Component
 public class TransactionConsumer {
 
-    private final TransactionConduit transactionConduit;
+    private final TransactionConduit conduit;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
-    public TransactionConsumer(TransactionConduit transactionConduit) {
-        this.transactionConduit = transactionConduit;
+    public TransactionConsumer(TransactionConduit conduit) {
+        this.conduit = conduit;
     }
 
     @KafkaListener(topics = "transactions", groupId = "midas")
     public void listen(String message) {
         try {
             Transaction transaction = objectMapper.readValue(message, Transaction.class);
-
             TransactionRecord record = new TransactionRecord(
                     transaction.getSenderId(),
                     transaction.getRecipientId(),
                     transaction.getAmount()
             );
-
-            transactionConduit.save(record);
+            conduit.save(record);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
-
